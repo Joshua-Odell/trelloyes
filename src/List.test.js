9dc5cd74ca5d094a7e98599c5827dba6e823dea7
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-06-30 15:13:28 -0500

    added snapshot testing

--- src/List.test.js
+++ src/List.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import List from './List';
+
 
 it('renders without crashing', () => {
     const div  = document.createElement('div');
     ReactDOM.render(<List />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the UI as expected', () => {
+    const tree = renderer
+        .create( <section className='List' />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
 });
 
