4b76aca565a2c606540bd616c135b2bffd001eaa
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-06-30 15:27:50 -0500

    added a snapshot test for Card.js

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import Card from './Card'
 
 it ('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Card />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it ('snapshot of card matches', () => {
+    const tree = renderer
+        .create(<div className='Card' />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();    
 });
